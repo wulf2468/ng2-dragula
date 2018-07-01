66cfaf80ae5d4b9c28b942bf4388b35cbcaf6e40
Wulf <7006958+wulf2468@users.noreply.github.com>
2018-07-01 08:55:20 -0500

    Added in an npmignore for node_modules Added missing files

---
+++ components/dragula.directive.ts
@@ -0,0 +1,66 @@
+import { Directive, Input, ElementRef, OnInit, OnChanges, SimpleChange } from '@angular/core';
+import { DragulaService } from './dragula.provider';
+import { dragula } from './dragula.class';
+
+@Directive({selector: '[dragula]'})
+export class DragulaDirective implements OnInit, OnChanges {
+  @Input() public dragula: string;
+  @Input() public dragulaModel: any;
+  @Input() public dragulaOptions: any = {};
+  private container: any;
+  private drake: any;
+  private isLocalMirror: boolean = false;
+
+  private el: ElementRef;
+  private dragulaService: DragulaService;
+  public constructor(el: ElementRef, dragulaService: DragulaService) {
+    this.el = el;
+    this.dragulaService = dragulaService;
+    this.container = el.nativeElement;
+  }
+
+  @Input() set dragulaLocalMirror(val: any) {
+    this.isLocalMirror = val !== 'false';
+  }
+
+  public ngOnInit(): void {
+    // console.log(this.bag);
+    let bag = this.dragulaService.find(this.dragula);
+    let checkModel = () => {
+      if (this.dragulaModel) {
+        if (this.drake.models) {
+          this.drake.models.push(this.dragulaModel);
+        } else {
+          this.drake.models = [this.dragulaModel];
+        }
+      }
+    };
+    if (bag) {
+      this.drake = bag.drake;
+      checkModel();
+      this.drake.containers.push(this.container);
+    } else {
+      if (this.isLocalMirror) {
+        this.dragulaOptions.mirrorContainer = this.el.nativeElement;
+      }
+      this.drake = dragula([this.container], Object.assign({}, this.dragulaOptions));
+      checkModel();
+      this.dragulaService.add(this.dragula, this.drake);
+    }
+  }
+
+  public ngOnChanges(changes: {dragulaModel?: SimpleChange}): void {
+    // console.log('dragula.directive: ngOnChanges');
+    // console.log(changes);
+    if (changes && changes.dragulaModel) {
+      if (this.drake) {
+        if (this.drake.models) {
+          let modelIndex = this.drake.models.indexOf(changes.dragulaModel.previousValue);
+          this.drake.models.splice(modelIndex, 1, changes.dragulaModel.currentValue);
+        } else {
+          this.drake.models = [changes.dragulaModel.currentValue];
+        }
+      }
+    }
+  }
+}
